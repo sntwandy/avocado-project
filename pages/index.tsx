7a2f577da61e30b93e5f88581f0184bd01560c61
sntwandy <sntwandy@gmail.com>
2020-08-22 18:03:27 -0400

    Working with the API on product[id] page and home[allProducts] page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data, length }) => setProductList(data))
+      .catch((error) => console.error(error.message))
+  }, [])
+
   return (
     <div>
       <Navbar />
       <div>Platzi and Next.js!</div>
+      {productList.map((product) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
     </div>
   )
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
-  const { query } = useRouter()
+  const [avo, setAvo] = useState<TProduct>()
+  const {
+    query: { id },
+  } = useRouter()
+
+  useEffect(() => {
+    id &&
+      window
+        .fetch(`/api/avo/${id}`)
+        .then((response) => response.json())
+        .then(({ data }) => setAvo(data))
+        .catch((error) => console.error(error.message))
+  }, [id])
 
   return (
     <section>
-      <h1>Página producto: {query.id}</h1>
+      <h1>Página producto.</h1>
+      <h3>{avo?.name}</h3>
+      <p>{avo?.attributes.description}</p>
     </section>
   )
 }
